b544ef6f07d7755fff8c999ddbc1dc34bf065853
Ekep Obasi <arreyettaekep@gmail.com>
2023-06-27 01:06:28 +0100

    Updated apiKey

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 const options = { method: 'GET', headers: { accept: 'application/json' } };
+const apiKey="a41defbf86-01f1fb7d60-rwvx04"
 
 const getRates = (defaultCurrency) =>
   fetch(
-    `https://api.fastforex.io/fetch-all?from=${defaultCurrency}&api_key=8bc9d4a802-a8e29af3dc-rppe6l`,
+    `https://api.fastforex.io/fetch-all?from=${defaultCurrency}&api_key=${apiKey}`,
     options
   ).then((response) => response.json());
 
 const convertCurrency = (from, to, amount) =>
   fetch(
-    `https://api.fastforex.io/convert?from=${from}&to=${to}&amount=${amount}&api_key=8bc9d4a802-a8e29af3dc-rppe6l`,
+    `https://api.fastforex.io/convert?from=${from}&to=${to}&amount=${amount}&api_key=${apiKey}`,
     options
   ).then((response) => response.json());
 
